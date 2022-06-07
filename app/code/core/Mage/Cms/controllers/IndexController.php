22e0465872acd344778eacc88e0309a2b0cd6932
Danijel Drazetic <daddo.1009@gmail.com>
2022-06-07 11:35:18 +0200

    initial commit

--- app/code/core/Mage/Cms/controllers/IndexController.php
+++ app/code/core/Mage/Cms/controllers/IndexController.php
@@ -41,6 +41,11 @@ class Mage_Cms_IndexController extends Mage_Core_Controller_Front_Action
      */
     public function indexAction($coreRoute = null)
     {
+//        echo Mage::getstoreConfig('some/random/xpath');
+//        var_dump( Mage::getConfig()->getNode('default/some/random/xpath'));
+
+//            echo get_class(Mage::helper('sales'));
+//        die;
         $pageId = Mage::getStoreConfig(Mage_Cms_Helper_Page::XML_PATH_HOME_PAGE);
         if (!Mage::helper('cms/page')->renderPage($this, $pageId)) {
             $this->_forward('defaultIndex');
